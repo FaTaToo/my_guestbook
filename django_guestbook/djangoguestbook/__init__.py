f30f2bb284c7a89f09c8bca3b0cad8431e0efa98
tungpt <tungpt@aoi-sys.vn>
2015-05-18 16:53:42 +0700

    Create api, PUT method is not work, need to check again.

--- django_guestbook/djangoguestbook/__init__.py
+++ django_guestbook/djangoguestbook/__init__.py
@@ -1,3 +1,24 @@
-# - Limit post message (10 letters), validate with Django Formview
-# - Owner and administrator can edit message
-# - Show updated_by and updated_date on view
+#
+# GET /api/guestbook/<guestbook_name>/greeting
+#     return JSON: guestbookname as STRING, more as BOOL, next_cursor as STRING,
+# 20 lastest greetings
+#     GET /api/guestbook/<guestbook_name>/greeting?cursor=<urlsafe_next_cursor>
+#         return 20 next greetings
+#     return Http 404 if query error
+# POST /api/guestbook/<guestbook_name>/greeting
+#     Create new greeting
+#     Successful return Http 204
+#     Fail return Http 404
+#     Form invalid return Http 400
+# GET /api/guestbook/<guestbook_name>/greeting/<id>
+#     return JSON: greeting id, content, date, updated_by, updated_date, guestbook_name
+#     return Http 404 if cannot retrieve
+# PUT /api/guestbook/<guestbook_name>/greeting/<id>
+#     update date greeting via parameters same as POST
+#     Successful return Http 204
+#     Fail return Http 404
+#     Form invalid return Http 400
+# DELETE /api/guestbook/<guestbook_name>/greeting/<id>
+#     delete greeting
+#     Successful return Http 204
+#     Fail return Http 404
