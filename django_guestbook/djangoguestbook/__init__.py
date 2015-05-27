8969a1cab8da7261a6e801e37ad4db4d00d2e650
tungpt <tungpt@aoi-sys.vn>
2015-05-27 09:35:22 +0700

    Unit test

--- django_guestbook/djangoguestbook/__init__.py
+++ django_guestbook/djangoguestbook/__init__.py
@@ -1,24 +1,26 @@
+#Implement unit test for models.py
+
+		# guestbook/tests/test_models.py
+		#
+		# class ModuleA(){
+		#   methodA();
+		# }
+		#
+		# class TestBassClass(object){
+		#   ... common init
+		# }
+		#
+		# class TestModuleA(TestBassClass){
+		#   test_methodA();
+		# }
+
+# Using all of these
 #
-# GET /api/guestbook/<guestbook_name>/greeting
-#     return JSON: guestbookname as STRING, more as BOOL, next_cursor as STRING,
-# 20 lastest greetings
-#     GET /api/guestbook/<guestbook_name>/greeting?cursor=<urlsafe_next_cursor>
-#         return 20 next greetings
-#     return Http 404 if query error
-# POST /api/guestbook/<guestbook_name>/greeting
-#     Create new greeting
-#     Successful return Http 204
-#     Fail return Http 404
-#     Form invalid return Http 400
-# GET /api/guestbook/<guestbook_name>/greeting/<id>
-#     return JSON: greeting id, content, date, updated_by, updated_date, guestbook_name
-#     return Http 404 if cannot retrieve
-# PUT /api/guestbook/<guestbook_name>/greeting/<id>
-#     update date greeting via parameters same as POST
-#     Successful return Http 204
-#     Fail return Http 404
-#     Form invalid return Http 400
-# DELETE /api/guestbook/<guestbook_name>/greeting/<id>
-#     delete greeting
-#     Successful return Http 204
-#     Fail return Http 404
+#     pytest (unit test)
+#         http://pytest.org/latest/
+#     mock (fake module)
+#         http://www.voidspace.org.uk/python/mock/
+#     GAE Testbed (fake datastore)
+#         google.appengine.ext.testbed
+
+# Don't commit dependencies to git, create requirements.txt to easy install all dependencies with command line
